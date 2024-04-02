a6a7402e2e7506eae419f01de4cb2c323add26b4
Mikolaj Stefaniak <mstefaniak@google.com>
2024-04-02 11:11:28 +0200

    Added prometheus metrics to go-rest app

--- go-rest/go.mod
+++ go-rest/go.mod
@@ -3,12 +3,20 @@ module github.com/mikouaj/go-rest
 go 1.20
 
 require (
-	github.com/go-logr/logr v1.2.4
-	github.com/go-logr/zapr v1.2.4
-	go.uber.org/zap v1.24.0
+	github.com/go-logr/logr v1.4.1
+	github.com/go-logr/zapr v1.3.0
+	go.uber.org/zap v1.27.0
 )
 
 require (
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/prometheus/client_golang v1.19.0 // indirect
+	github.com/prometheus/client_model v0.5.0 // indirect
+	github.com/prometheus/common v0.48.0 // indirect
+	github.com/prometheus/procfs v0.12.0 // indirect
 	go.uber.org/atomic v1.11.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
+	golang.org/x/sys v0.16.0 // indirect
+	google.golang.org/protobuf v1.32.0 // indirect
 )
--- go-rest/internal/controller/controller.go
+++ go-rest/internal/controller/controller.go
@@ -2,34 +2,66 @@
 package controller
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/go-logr/logr"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 type controller struct {
-	log          logr.Logger
-	pathHandlers map[string]http.HandlerFunc
+	log            logr.Logger
+	pathHandlers   map[string]http.Handler
+	reqTotalMetric *prometheus.CounterVec
+}
+
+type auditedResponseWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func (w *auditedResponseWriter) WriteHeader(code int) {
+	w.statusCode = code
+	w.ResponseWriter.WriteHeader(code)
 }
 
 func New() *controller {
-	return &controller{
-		pathHandlers: map[string]http.HandlerFunc{
-			"/healthz": healthzHandler,
-			"/books":   booksHandler,
-		},
+	c := controller{
+		pathHandlers: make(map[string]http.Handler),
+		reqTotalMetric: promauto.NewCounterVec(prometheus.CounterOpts{
+			Name: "go_rest_http_requests_total",
+			Help: "The total number of HTTP requests",
+		}, []string{"path", "status"}),
 	}
+	c.RegisterPathHandler("/healthz", http.HandlerFunc(healthzHandler))
+	c.RegisterPathHandler("/books", c.wrapHandlerWithAudit(http.HandlerFunc(booksHandler)))
+	c.RegisterPathHandler("/metrics", promhttp.Handler())
+	return &c
 }
 
 func (c *controller) SetLogger(log logr.Logger) {
 	c.log = log
 }
 
+func (c *controller) RegisterPathHandler(path string, handler http.Handler) {
+	c.pathHandlers[path] = handler
+}
+
 func (c *controller) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	c.log.V(2).WithValues("remoteAddr", r.RemoteAddr).Info("Handling request")
-	handlerFunc := http.NotFound
-	if f, ok := c.pathHandlers[r.URL.Path]; ok {
-		handlerFunc = f
+	if h, ok := c.pathHandlers[r.URL.Path]; ok {
+		h.ServeHTTP(w, r)
+	} else {
+		http.NotFound(w, r)
 	}
-	handlerFunc(w, r)
+}
+
+func (c *controller) wrapHandlerWithAudit(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		auditedWriter := &auditedResponseWriter{ResponseWriter: w}
+		handler.ServeHTTP(auditedWriter, r)
+		c.reqTotalMetric.With(prometheus.Labels{"path": r.URL.Path, "status": fmt.Sprint(auditedWriter.statusCode)}).Inc()
+	})
 }
